9cde6edc356dd9ba30b0b4c0048e2dcce5440d8a
Oskar Kupski <oskar.kupski@gmail.com>
2023-09-03 19:24:00 +0200

    extracted `<PostCardList />`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { PageSection } from '@/components';
-import { PostCard } from '@/posts';
 import classNames from 'classnames';
+import { PostCardList } from '@/posts';
 
 export default function HomePage() {
   return (
@@ -17,13 +17,7 @@ export default function HomePage() {
           className="mb-10"
         />
 
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          <PostCard />
-
-          <PostCard />
-
-          <PostCard />
-        </div>
+        <PostCardList />
       </div>
     </main>
   );
